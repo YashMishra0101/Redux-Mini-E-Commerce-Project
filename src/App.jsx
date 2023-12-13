9bf91f189bf0f11c179a1804e3c1d1e6cd5ce692
YashMishra0101 <yashrkmfedu6927@gmail.com>
2023-12-13 23:54:16 +0530

    basic-setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-green-500 text-center mt-2'>Testing</div>
+  )
+}
+
+export default App
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li><img src="" alt="logo" /></li>
+                <li>Home</li>
+                <li><img src="" alt="cart-logo" /></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default NavBar
